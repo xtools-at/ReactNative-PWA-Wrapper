537c52269785d208aa8f787a76b2b63c7737fc2a
Martin Kainzbauer <m.kainzbauer@xtools.at>
2017-10-15 20:24:03 +0200

    started working

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+/**
+ * Main Component
+ */
+
+import React, { Component } from 'react';
+import WebView from './WebView';
+
+export default class Main extends Component<{}> {
+  render() {
+    return (
+      <WebView />
+    );
+  }
+}
